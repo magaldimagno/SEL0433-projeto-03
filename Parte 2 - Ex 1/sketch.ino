5fa636fa384ae96886f68edb7de5781bf6da78e3
Eduardo Magno <edumagno@usp.br>
2026-06-22 22:07:25 -0300

    Parte 02 - Exercício 1

---
+++ Parte 2 - Ex 1/sketch.ino
@@ -0,0 +1,33 @@
+#include <ESP32Servo.h>
+
+// Pinos de sinal
+#define servopin  2
+#define potenciometropin  4
+
+// Inicializa o objeto Servo
+Servo servo;
+
+// Posição inicial
+int pos = 0;
+
+void setup() {
+  Serial.begin(115200);
+  ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+  servo.setPeriodHertz(50);
+  servo.attach(servopin, 500, 2400);
+  pinMode(potenciometropin, INPUT);
+}
+
+void loop() {
+  int potenciometro = analogRead(potenciometropin); 
+  
+  pos = map(potenciometro, 0, 4095, 0, 180); // Regra de três para determinar o valor do dutycycle do PWM.
+  
+  servo.write(pos);
+  delay(15);
+  Serial.print("Angulo Servo: ");
+  Serial.println(pos);
+}
